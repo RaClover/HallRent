699a0a4cf684da45cdad6d267e293a9ac644abd8
RaheebGo <Raheeb772652540@gmail.com>
2023-04-28 04:38:35 +0300

    added the UsersTableSeeder.php

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        User::factory(10)->create();
+    }
+}
