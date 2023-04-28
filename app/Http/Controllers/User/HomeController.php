bf07220dd0330207c4d4bdba8c336537490b49c9
RaheebGo <Raheeb772652540@gmail.com>
2023-04-28 04:41:39 +0300

    added some functions

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
+use function Sodium\add;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        return inertia('Welcome');
+        $address = Address::select('city', DB::raw('COUNT(halls.id) AS hallCount'))->leftJoin('halls', 'addresses.hall_id', '=', 'halls.id')->groupBy('city')->paginate(4);
+
+
+
+        return inertia('Welcome' , [
+            'address' => $address
+        ]);
     }
 
+
+    public function aboutUs()
+    {
+     return Inertia::render('User/AboutUs');
+    }
 
 
 
