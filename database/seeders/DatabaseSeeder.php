70d965f90c1cf73bc4d0eac4e80c3e84751c9e26
RaheebGo <Raheeb772652540@gmail.com>
2023-04-28 04:40:59 +0300

    Edited the DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,18 +12,12 @@ use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-
-         User::factory(2)->has(
-             Hall::factory()->has(
-                 Address::factory()->count(1)
-             )->count(1)
-         )->create();
-
-
+        User::factory(1)->has(
+            Hall::factory()->has(
+                Address::factory()->count(1)
+            )->count(2)
+        )->create();
     }
 }
