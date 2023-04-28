28caf60f2df96e2eeceb31df08068e233ea22c48
RaheebGo <Raheeb772652540@gmail.com>
2023-04-28 04:34:51 +0300

    added the about component

---
+++ resources/js/Pages/User/AboutUs.vue
@@ -0,0 +1,19 @@
+
+<script>
+import NavBarLayout from "@/Layouts/User/NavBarLayout.vue";
+
+export default {
+    name: "AboutUs",
+    components: {NavBarLayout}
+}
+</script>
+
+<template>
+<NavBarLayout>
+    <h1>This is about us page</h1>
+</NavBarLayout>
+</template>
+
+<style scoped>
+
+</style>
