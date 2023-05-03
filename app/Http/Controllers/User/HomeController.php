bbe7f1706ca5fd4ee3b1b90b558cbfccb5206ded
RaheebGo <Raheeb772652540@gmail.com>
2023-05-03 14:49:58 +0300

    edited some code

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -4,25 +4,29 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Address;
+use App\Models\Hall;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
-use function Sodium\add;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        $address = Address::select('city', DB::raw('COUNT(halls.id) AS hallCount'))->leftJoin('halls', 'addresses.hall_id', '=', 'halls.id')->groupBy('city')->paginate(4);
-
-
-
-        return inertia('Welcome' , [
-            'address' => $address
+        $address = Address::select('city', DB::raw('COUNT(halls.id) AS hallCount'))
+            ->leftJoin('halls', 'addresses.hall_id', '=', 'halls.id')
+            ->groupBy('city')
+            ->paginate(4);
+        $cities = Address::select('city')->groupBy('city')->get()->pluck('city');
+        return Inertia::render('Welcome', [
+            'address' => $address ,
+            'cities' => $cities
         ]);
     }
+
 
 
     public function aboutUs()
