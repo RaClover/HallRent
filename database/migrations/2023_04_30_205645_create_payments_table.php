a4bde1b6ca6b422fb10268cf327c3bfba0a3ce12
RaheebGo <Raheeb772652540@gmail.com>
2023-05-03 14:45:27 +0300

    created Payment table

---
+++ database/migrations/2023_04_30_205645_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('booking_id')->unsigned();
+            $table->decimal('amount', 8, 2);
+            $table->string('payment_method');
+            $table->date('payment_date');
+            $table->timestamps();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
