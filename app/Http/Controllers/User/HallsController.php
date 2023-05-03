86c2c2e699b629100a6b97b7e0ba0378928e1afc
RaheebGo <Raheeb772652540@gmail.com>
2023-05-03 14:49:04 +0300

    added the HallsController.php

--- app/Http/Controllers/User/HallsController.php
+++ app/Http/Controllers/User/HallsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use App\Models\Hall;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -11,15 +12,28 @@ class HallsController extends Controller
 {
     public function index($city)
     {
-        $halls = Hall::whereHas('address', function ($query) use ($city) {
-            $query->where('city', $city);
-        })->paginate(18);
+
+        $address = Address::where('city', $city)->first();
+
+        if (!$address) {
+            return redirect()->back()->with('message' , 'there is no halls in ' . $city . ' city yet');
+        }
+
+        $halls = Hall::join('addresses', 'halls.id', '=', 'addresses.hall_id')
+            ->where('addresses.city', $city)
+            ->paginate(18);
         $links = $halls->links()->toHtml();
         return Inertia::render('User/Halls' , [
             'halls' => $halls ,
             'city' => $city ,
             'links' => $links,
         ]);
+    }
+
+
+    public function hallDetail()
+    {
+        return Inertia::render('User/HallDetail');
     }
 
 
