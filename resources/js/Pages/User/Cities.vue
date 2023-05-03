7b336f439fee8d54fc50369090eb3a83b9eb61bf
RaheebGo <Raheeb772652540@gmail.com>
2023-05-03 14:46:51 +0300

    added Cities.vue components for cities list

---
+++ resources/js/Pages/User/Cities.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <li v-for="city in cities" :key="city">{{ city }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        cities: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
