792ce103477a54bb57dafe63d48568b7a74ff3cd
RaheebGo <Raheeb772652540@gmail.com>
2023-05-05 03:32:21 +0300

    added some code

--- app/Http/Controllers/User/HallsController.php
+++ app/Http/Controllers/User/HallsController.php
@@ -5,30 +5,52 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Address;
 use App\Models\Hall;
+use App\Models\Type;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Http\Resources\HallResource;
+use App\Http\Resources\TypeResource;
 
 class HallsController extends Controller
 {
-    public function index($city)
+    public function index(Request $request , $city)
     {
+        $halls = HallResource::collection(
+            Hall::query()
+                ->join('addresses', 'halls.id', '=', 'addresses.hall_id')
+                ->withTypes($request->type ?? '')
+                ->withMinPrice($request->min_price ?? '')
+                ->withMaxPrice($request->max_price ?? '')
+                ->withSearch($request->search ?? '')
+                ->orderBy($request->sortBy ?? 'name')
+                ->with('type')
+                ->where('addresses.city', $city)
+                ->paginate(8)
+                ->withQueryString()
+        );
 
-        $address = Address::where('city', $city)->first();
-
-        if (!$address) {
-            return redirect()->back()->with('message' , 'there is no halls in ' . $city . ' city yet');
-        }
-
-        $halls = Hall::join('addresses', 'halls.id', '=', 'addresses.hall_id')
-            ->where('addresses.city', $city)
-            ->paginate(18);
+        $types = TypeResource::collection(Type::withCount('halls')->get());
         $links = $halls->links()->toHtml();
         return Inertia::render('User/Halls' , [
             'halls' => $halls ,
             'city' => $city ,
             'links' => $links,
+            'types' => $types,
+            'category' => $request->category ?? '',
+            'min_price' => $request->min_price ?? '',
+            'max_price' => $request->max_price ?? '',
+            'sortBy' => $request->sortBy ?? '',
+            'search' => $request->search
         ]);
     }
+
+
+
+
+
+
+
+
 
 
     public function hallDetail()
