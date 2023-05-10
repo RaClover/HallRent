683116d5d1a743dfa10638659d3db461d183d040
RaheebGo <Raheeb772652540@gmail.com>
2023-05-11 02:30:16 +0300

    created the HallImage.php model

--- app/Models/HallImage.php
+++ app/Models/HallImage.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HallImage extends Model
 {
     use HasFactory;
+
+    public function hall():BelongsTo
+    {
+        return $this->belongsTo(Hall::class);
+    }
 }
