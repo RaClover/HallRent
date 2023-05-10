a7e7f8bd8979478bf00bc6e80dd28ade9e8649ce
RaheebGo <Raheeb772652540@gmail.com>
2023-05-11 02:31:19 +0300

    added some code in HallsTableSeeder.php

--- database/seeders/HallsTableSeeder.php
+++ database/seeders/HallsTableSeeder.php
@@ -18,20 +18,20 @@ class HallsTableSeeder extends Seeder
 
     public function run()
     {
-
-        $types = Type::whereIn('slug', ['children-birthday', 'birthday', 'wedding', 'new-year', 'corporate', 'graduation', 'family-reunion', 'special-event'])->get();
-        $users = User::where('role', 'partner')->get();
-        $addresses = Address::all();
-
-        foreach ($types as $type) {
-            foreach ($users as $user) {
-                $halls = Hall::factory()->count(1)->create([
-                    'user_id' => $user->id,
-                    'type_id' => $type->id,
-                    'address_id' => $addresses->random()->id,
-                ]);
-            }
-        }
+//
+//        $types = Type::whereIn('slug', ['children-birthday', 'birthday', 'wedding', 'new-year', 'corporate', 'graduation', 'family-reunion', 'special-event'])->get();
+//        $users = User::where('role', 'partner')->get();
+//        $addresses = Address::all();
+//
+//        foreach ($types as $type) {
+//            foreach ($users as $user) {
+//                $halls = Hall::factory()->count(1)->create([
+//                    'user_id' => $user->id,
+//                    'type_id' => $type->id,
+//                    'address_id' => $addresses->random()->id,
+//                ]);
+//            }
+//        }
 
 
 
@@ -45,34 +45,34 @@ class HallsTableSeeder extends Seeder
           */
 
 
-//        $hallsData = [];
-//
-//        $types = Type::whereIn('slug', ['childrens-birthday', 'birthday', 'wedding', 'new-year', 'corporate', 'graduation', 'family-reunion', 'special-event'])->get();
-//        $users = User::where('role', 'partner')->get();
-//        $addresses = Address::all();
-//
-//        foreach ($types as $type) {
-//            foreach ($users as $user) {
-//                $hallsData[] = [
-//                    'name' => 'Hall ' . $type->slug . ' for ' . $user->name,
-//                    'slug' => Str::slug('Hall ' . $type->slug . ' for ' . $user->name),
-//                    'description' => 'This is a ' . $type->name . ' hall owned by ' . $user->name,
-//                    'email' => $user->email,
-//                    'phone' => $user->phone,
-//                    'price' => rand(100, 1000),
-//                    'size' => rand(50, 500),
-//                    'created_at' => now(),
-//                    'updated_at' => now(),
-//                ];
-//            }
-//        }
-//
-//        foreach ($hallsData as $key => $hallData) {
-//            $hallData['type_id'] = $types[$key % count($types)]->id;
-//            $hallData['user_id'] = $users[$key % count($users)]->id;
-//            $hallData['address_id'] = $addresses[$key % count($addresses)]->id;
-//            Hall::create($hallData);
-//        }
+        $hallsData = [];
+
+        $types = Type::whereIn('slug', ['childrens-birthday', 'birthday', 'wedding', 'new-year', 'corporate', 'graduation', 'family-reunion', 'special-event'])->get();
+        $users = User::where('role', 'partner')->get();
+        $addresses = Address::all();
+
+        foreach ($types as $type) {
+            foreach ($users as $user) {
+                $hallsData[] = [
+                    'name' => 'Hall ' . $type->slug . ' for ' . $user->name,
+                    'slug' => Str::slug('Hall ' . $type->slug . ' for ' . $user->name),
+                    'description' => 'This is a ' . $type->name . ' hall owned by ' . $user->name,
+                    'email' => $user->email,
+                    'phone' => $user->phone,
+                    'price' => rand(100, 1000),
+                    'size' => rand(50, 500),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+        }
+
+        foreach ($hallsData as $key => $hallData) {
+            $hallData['type_id'] = $types[$key % count($types)]->id;
+            $hallData['user_id'] = $users[$key % count($users)]->id;
+            $hallData['address_id'] = $addresses[$key % count($addresses)]->id;
+            Hall::create($hallData);
+        }
 
 
 
