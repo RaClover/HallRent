2ecd0d8103575b5182536acad7b8fe992cbf3b7f
RaheebGo <Raheeb772652540@gmail.com>
2023-05-25 08:34:24 +0300

    edited some data

---
+++ resources/js/Pages/User/Booking/StepContentRight.vue
@@ -0,0 +1,126 @@
+<script>
+export default {
+    components : {
+    },
+    props: {
+        hall: Object,
+        type: Object,
+        images: Object,
+        address: Object,
+        date:String,
+        startTime:String,
+        endTime:String,
+    }
+}
+</script>
+
+<template>
+    <div class="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32 ">
+        <slot/>
+        <div class="bg-white px-1 pt-1 lg:mt-0 mb-40 ml-40 ">
+
+            <!--               here is the image -->
+            <div class="p-4  dark:bg-gray-50 rounded-md shadow-sm">
+                <div class="relative  w-full h-60 mb-2">
+                    <img src="https://i.postimg.cc/RF9h9qzx/pexels-jaime-reimer-2695232.jpg" alt=""
+                         class="object-cover w-full h-full rounded">
+                    <span class="absolute top-0 right-0 px-2 py-1 mt-2 mr-2 text-xs text-white bg-white">
+                             <span class="flex  text-orange-500 gap-x-4 p-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-star-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-star-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-star-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-star-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-star" viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                                    </svg>
+                                </span>
+                    </span>
+                </div>
+                <div class="flex items-center justify-between mb-4">
+                    <div>
+                        <h2 class="text-lg font-semibold dark:text-gray-800">{{hall.name}}</h2>
+                    </div>
+                    <div class="text-base font-semibold text-gray-600">${{hall.price}}/hr</div>
+                </div>
+
+                <div class="flex items-center  mb-4">
+                    <a href="" class="mr-1 text-blue-600 dark:text-gray-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor"
+                             class="bi bi-geo-alt" viewBox="0 0 16 16">
+                            <path
+                                d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                        </svg>
+                    </a>
+                    <span class="text-sm text-gray-600 dark:text-black">{{address.street}}, {{address.city}}</span>
+                </div>
+
+                <div class="flex items-center justify-between mb-2">
+                    <h2 class="text-xl font-bold dark:text-black">Date & Time</h2>
+
+                </div>
+                <div class="flex items-center justify-between mb-2">
+                    <h2 class="text-md dark:text-black">{{date}}</h2>
+
+                </div>
+                <div class="flex items-center justify-between mb-4">
+                    <h2 class="text-md  dark:text-black">{{startTime}} - {{endTime}} </h2>
+
+                </div>
+
+                <div class="mt-6 border-t py-2">
+
+                    <div class="flex items-center justify-between mb-2">
+                        <h2 class="text-xs font-medium dark:text-black">Day</h2>
+                        <span class="inline-block px-2 py-1 text-xs text-gray-600 dark:text-black">6 night 7 days
+                        </span>
+                    </div>
+
+                    <div class="flex items-center justify-between mb-2">
+                        <h2 class="text-xs font-medium dark:text-black">Day</h2>
+                        <span class="inline-block px-2 py-1 text-xs text-gray-600 dark:text-black">6 night 7 days
+                        </span>
+                    </div>
+
+                    <div class="flex items-center justify-between mb-2">
+                        <h2 class="text-xs font-medium dark:text-black">Day</h2>
+                        <span class="inline-block px-2 py-1 text-xs text-gray-600 dark:text-black">6 night 7 days
+                        </span>
+                    </div>
+                </div>
+                <!-- Total -->
+                <div class="mt-1 border-t    py-2">
+                    <div class="flex items-center justify-between py-4 px-10 mb-14 leading-8 bg-white font-heading font-medium rounded-3xl">
+                        <span>Total</span>
+                        <span class="flex items-center text-xl text-blue-500">
+                        <span class="mr-2 text-base">$</span>
+                        <span>720,70</span>
+                    </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
